438bdc81cd08ca2543186a30f29bc55bc050af6c
Alvaro Lomas <alomas@est.ups.edu.ec>
2020-12-15 16:57:24 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,14 @@
+# Script de prueba para Git
+# Autor: Alvaro Lomas <alomas@est.ups.edu.ec> 
+# Fecha: 15-12-2020
+# Versión: 1.0.0
+
+def main():
+    print("¡Hola, mundo!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
